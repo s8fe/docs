56b53bca683a338222b9ba13ead6831e70f0a477
Christoph Richter <christoph@richter.md>
2022-08-12 16:16:27 +0200

    fixing broken link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,8 +84,8 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Docs',
+                to: 'docs/intro',
               },
             ],
           },
